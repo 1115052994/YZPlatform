ff8040ffed1d4d2759bec53754dd58ae8050ab9e
Jiang <872389893@qq.com>
2018-04-16 10:45:58 +0800

    Login

--- app/src/main/java/com/plt/yzplatform/activity/LoginActivity.java
+++ app/src/main/java/com/plt/yzplatform/activity/LoginActivity.java
@@ -1,8 +1,10 @@
-package com.plt.yzplatform;
+package com.plt.yzplatform.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.plt.yzplatform.R;
 
 public class LoginActivity extends AppCompatActivity {
 
